316649c83e69af47124ce358511c9b739dee363c
Gamerman2001 <Tro12185@gmail.com>
2019-06-07 16:01:48 -0400

    bombbbed

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,18 +3,47 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
+//SUSHI CONTAINER AND TABLE ARE CHILD TO APP
+//SUSHI Container is parent to SUSHI and MOREButton
+//GET Sushi List from Server
+//ONly 4 sushi are displayed at a time
+//Clicking "More Sushi" shows next 4 Sushi in list 
+//Clicking Sushi on a plate Eats the Sushi, it will be removed after eating the sushi
+      //an empty plate will appear on the sushi that was eaten
+//Have a state that is Money
+//When the Sushi is eaten charge customer for Sushi *In TABLE Component*
+//When the money is Done no more sushi can be eaten
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sushi: [], 
+      money: 150
+    }
+
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/sushis')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson)=> {
+    this.setState({
+      sushi: myJson
+    })
+  });
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushi={this.state.sushi}  />
+        <Table money={this.state.money}/>
       </div>
     );
   }
 }
-
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+
+  let sushiLi = props.sushi.map((sushi)=> {
+      let name = sushi.name
+      let img_url = sushi.img_url
+      let price = sushi.price
+      let id = sushi.id
+      return <div><Sushi name={name} img_url={img_url} price={price} id={id} /></div>
+    })
+    console.log(sushiLi)
+
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        { 
+          
+        //rendering sushi here
+        
         }
         <MoreButton />
       </div>
